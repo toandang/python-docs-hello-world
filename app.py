b7bc519576a7de10187053eb61fd7608451504d0
Toan Dang Anh <toan.danganh@nashtechglobal.com>
2021-05-22 18:11:15 +0700

    update image

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
-import os
+# from flask import Flask, render_template
+# import os
 
-IMAGE_FOLDER = os.path.join('', 'images')
+# IMAGE_FOLDER = os.path.join('', 'images')
 
+# app = Flask(__name__)
+# app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+
+# @app.route('/')
+# @app.route('/index')
+# def show_index():
+#     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'ds_ai_logo.png')
+#     return render_template("index.html", user_image = full_filename)
+from flask import Flask, flash, request, redirect, url_for, render_template
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 
-@app.route('/')
-@app.route('/index')
-def show_index():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'ds_ai_logo.png')
-    return render_template("index.html", user_image = full_filename)
+@app.route("/")
+def hello():
+    # return "Hello, World!"
+	return redirect(url_for('', filename='images/' + 'ds_ai_logo.png'), code=301)
+
+
+# if __name__ == "__main__":
+#     app.run()
